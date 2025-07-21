7379c1e621301390cf413b88d8d7a7299782fd33
ahmedanwar1234 <ahmedanswar123@gmail.com>
2025-07-21 17:11:15 +0300

    authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,21 @@ import { Welcome } from "../welcome/welcome";
 import Navbar from "~/components/Navbar";
 import { resumes } from "constants/index";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { usePuterStore } from "lib/puter";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
+   const {auth}=usePuterStore();
+
+    const navigate=useNavigate()
+    
+useEffect(()=>{
+if(!auth.isAuthenticated)navigate('auth?next=/')
+},[auth.isAuthenticated])
+
+
+
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
@@ -14,7 +27,7 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   return <main className=" bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
-    <section className=" main-section ">
+    <section className=" main-section py-16 ">
       <div className="page-heading">
 <h1>Track Your Applicatinos & Resume Rationgs</h1>
 <h2 > Review your submissions and check AI-powerd feedback</h2>
