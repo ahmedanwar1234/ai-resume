f281bbe77bc5617347d193855b7b675eedf46268
ahmedanwar1234 <ahmedanswar123@gmail.com>
2025-07-25 19:06:00 +0300

    fimal

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,24 +7,27 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const handleLogout = async () => {
-    await auth.signOut();       // log out the user
-    navigate('/auth');         // redirect to login page
+    await auth.signOut(); // log out the user
+    navigate('/auth'); // redirect to login page
   };
 
   return (
-    <nav className='navbar flex justify-between items-center p-4 bg-white shadow-md'>
+    <nav className='navbar flex  sm:flex-row justify-between  items-center p-4 bg-white shadow-md gap-4 sm:gap-0'>
       <Link to='/' className='text-2xl font-bold text-gradient'>
         RESUMIND
       </Link>
 
-      <div className="flex items-center gap-4">
-        <Link to='/upload' className='primary-button text-nowrap max-sm: w-fit'>
+      <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-4 w-full sm:w-auto">
+        <Link
+          to='/upload'
+          className='primary-button w-full sm:w-fit text-center'
+        >
           Upload Resume
         </Link>
-        
+
         <button
           onClick={handleLogout}
-          className='bg-red-500 text-white px-4 py-2 rounded-2xl hover:bg-red-600 transition'
+          className='bg-red-500 text-white px-4 py-2 rounded-2xl hover:bg-red-600 transition w-full sm:w-fit'
         >
           Logout
         </button>
